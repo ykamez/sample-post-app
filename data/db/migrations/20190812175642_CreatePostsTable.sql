f4e6f3a09199e221566a424a5f978859eb52a822
Yuta Kamezaki <25625303+ykamez@users.noreply.github.com>
2019-08-12 17:58:41 +0900

    Create migration file

---
+++ data/db/migrations/20190812175642_CreatePostsTable.sql
@@ -0,0 +1,13 @@
+
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+create table posts (
+  id              serial primary key,
+  title           varchar(255),
+);
+
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+drop table posts;
+
