ef2040bdde88ea8f2e0f9749309d52fa21527723
Yuta Kamezaki <25625303+ykamez@users.noreply.github.com>
2019-08-12 19:18:23 +0900

    Add util

---
+++ app/util/post.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"context"
+
+	api_pb "github.com/ykamez/sample-post-app/api"
+	"github.com/ykamez/sample-post-app/infra/record"
+)
+
+// PostsToPb converts post objects to pb objects.
+func PostsToPb(ctx context.Context, posts []*record.Post) []*api_pb.Post {
+	pbs := make([]*api_pb.Post, 0, len(posts))
+	for i, m := range posts {
+		pbs[i] = PostToPb(ctx, m)
+	}
+	return pbs
+}
+
+// PostToPb converts post object to pb object.
+func PostToPb(ctx context.Context, p *record.Post) *api_pb.Post {
+	pbp := &api_pb.Post{
+		Id:    uint32(p.ID),
+		Title: p.Title.String,
+	}
+	return pbp
+}
